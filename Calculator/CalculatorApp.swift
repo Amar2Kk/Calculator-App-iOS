12a7d54f94d046877d4171bcadad1145a8f9d68e
Ammar Abbas <95864103+Amar2Kk@users.noreply.github.com>
2024-04-28 21:05:51 +0300

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Ammar Abbas on 28/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
