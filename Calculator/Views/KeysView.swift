2c583ab2e6372107cf5c581be4f07c79a2f91fff
Ammar Abbas <95864103+Amar2Kk@users.noreply.github.com>
2024-04-29 19:08:45 +0300

    Re-initialized the project

--- Calculator/Views/KeysView.swift
+++ Calculator/Views/KeysView.swift
@@ -1,18 +1,137 @@
-//
 //  KeysView.swift
 //  Calculator
 //
-//  Created by Ammar Abbas on 29/04/2024.
+//  Created by Ammar Abbas on 28/04/2024.
 //
 
 import SwiftUI
 
 struct KeysView: View {
+    
+    @State var value = "0"
+    @State var runningNumber = 0
+    @State var currentOperation: Operation = .none
+    @State private var changeColor = false
+    
+    let buttons : [[Keys]] = [
+        [.clear, .negative, .percent, .divide],
+        [.seven, .eight, .nine, .multiply],
+        [.four, .five, .six, .subtract],
+        [.one, .two, .three, .add],
+        [.zero, .decimal, .equal]
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Spacer()
+            HStack {
+                RoundedRectangle(cornerRadius: 10)
+                    .foregroundColor(.white)
+                    .frame(width: 350, height: 280)
+                    .overlay(Text(value)
+                        .bold()
+                        .font(.system(size: 100))
+                        .foregroundColor(.black)
+                    )
+            }.padding()
+            ForEach(buttons, id: \.self){ row in
+                HStack(spacing: 10){
+                    ForEach(row, id: \.self){ elem in
+                        Button {
+                            self.onClick(button: elem)
+                        } label: {
+                            Text(elem.rawValue)
+                                .font(.system(size: 40))
+                                .frame(width: self.calcWidth(elem: elem), height: self.clacHeight(elem: elem))
+                                .background(elem.buttonColor)
+                                .foregroundColor(.black)
+                                .cornerRadius(10)
+                        }
+                        
+                    }
+                }.padding(.bottom, 1)
+            }
+        }
     }
+    
+    func onClick(button: Keys){
+        
+        switch button {
+        case .add, .subtract, .multiply, .divide, .equal:
+            if button == .add {
+                self.currentOperation = .add
+                self.runningNumber = Int(self.value) ?? 0
+            }
+            else if button == .subtract {
+                self.currentOperation = .subtract
+                self.runningNumber = Int(self.value) ?? 0
+            }
+            else if button == .multiply {
+                self.currentOperation = .multiply
+                self.runningNumber = Int(self.value) ?? 0
+            }
+            else if button == .divide {
+                self.currentOperation = .divide
+                self.runningNumber = Int(self.value) ?? 0
+            }
+            else if button == .equal {
+                let runningValue = self.runningNumber
+                let currentValue = Int(self.value) ?? 0
+                switch self.currentOperation {
+                case .add:
+                    self.value = "\(runningValue + currentValue)"
+                case .subtract:
+                    self.value = "\(runningValue - currentValue)"
+                case .multiply:
+                    self.value = "\(runningValue * currentValue)"
+                case .divide:
+                    self.value = "\(runningValue / currentValue)"
+                case .none:
+                    break
+                }
+            }
+            if button != .equal {
+                self.value = "0"
+            }
+        case .clear: self.value = "0"
+        case .decimal:
+            if !self.value.contains(".") {
+                self.value += "."
+            }
+        case .negative:
+            if let number = Double(self.value) {
+                self.value = String(-number)
+            }
+        case .percent:
+            if let number = Double(self.value) {
+                self.value = String(number / 100)
+            }
+        default:
+            let number = button.rawValue
+            if self.value == "0" {
+                value = number
+            }
+            else{
+                self.value = "\(self.value)\(number)"
+            }
+        }
+    }
+    func calcWidth(elem:Keys) -> CGFloat {
+        if elem == .zero {
+            return (UIScreen.main.bounds.width - (3 * 10)) / 2
+        }
+        
+        return (UIScreen.main.bounds.width - (5 * 10)) / 4
+    }
+    
+    func clacHeight(elem: Keys) -> CGFloat {
+        return (UIScreen.main.bounds.width - (5 * 10)) / 4
+    }
+    
 }
+
 
 #Preview {
     KeysView()
 }
+
--- Calculator/Views/SplashScreenView.swift
+++ Calculator/Views/SplashScreenView.swift
@@ -8,8 +8,43 @@
 import SwiftUI
 
 struct SplashScreenView: View {
+    @State var isActive: Bool = false
+    @State private var size = 0.7
+    @State private var opacity = 0.4
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if isActive {
+            KeysView()
+        } else {
+            VStack{
+                Image("SplashLogo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 150)
+                    .foregroundColor(Color("Dark"))
+                Text("Calculate Your Way to")
+                    .font(.system(size: 30))
+                    .foregroundStyle(Color("Dark"))
+                Text("Precision")
+                    .font(.system(size: 30))
+                    .foregroundStyle(Color("Voperator"))
+            }
+            .scaleEffect(size)
+            .opacity(opacity)
+            .onAppear{
+                withAnimation(.easeIn(duration: 1.0)){
+                    self.size = 1.1
+                    self.opacity = 1.0
+                }
+            }
+            .onAppear{
+                DispatchQueue.main.asyncAfter(deadline: .now() + 3){
+                    withAnimation{
+                        self.isActive = true
+                    }
+                }
+            }
+        }
     }
 }
 
